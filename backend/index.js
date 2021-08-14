f69cf67030b5e5958abe8c0031ab8a59841f7e56
Lim Bing Sen <bingsenlim0806@gmail.com>
2021-08-14 15:38:58 +0800

    Changed backend index.js and successfully deployed to heroku

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-const helmet = require("helmet")
+const dotenv = require("dotenv");
+const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 let port = process.env.PORT || 8080;
@@ -22,7 +22,7 @@ dotenv.config();
 // Connet to database
 mongoose
   .connect(
-    process.env.MONGO_URL,
+    "mongodb+srv://bingsen:5mouseketeers@cluster0.ag8nj.mongodb.net/LIT2021?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -32,18 +32,38 @@ mongoose
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
 
-  // Middleware
-  app.use(cors());
-  app.use(express.json());
-  app.use(helmet());
-  app.use(morgan("common"));
+//setup for production environment
+if (process.env.NODE_ENV === "production") {
+  console.log("environment is production!");
+  console.log("__dirname is: " + __dirname);
+  app.use(express.static(path.join(__dirname, "/dist")));
 
-  // Routes
-  app.use("/api/auth", authRoute);
-  app.use("/api/users", userRoute);
-  app.use("/api/informations", infoRoute);
-  app.use("/api/assistances", assistRoute);
-
-  server.listen(port, () => {
-      console.log("Server listening on port: " + port);
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/dist", "index.html"));
   });
+  app.get("/home", function (req, res) {
+    res.sendFile(path.join(__dirname, "/dist", "index.html"));
+  });
+  app.get("/assistance", function (req, res) {
+    res.sendFile(path.join(__dirname, "/dist", "index.html"));
+  });
+  app.get("/contact", function (req, res) {
+    res.sendFile(path.join(__dirname, "/dist", "index.html"));
+  });
+}
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/informations", infoRoute);
+app.use("/api/assistances", assistRoute);
+
+server.listen(port, () => {
+  console.log("Server listening on port: " + port);
+});
