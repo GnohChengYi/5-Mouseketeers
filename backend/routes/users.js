034fe92211b12ebc4d0dd94f483ab4b9138991a2
Lim Bing Sen <bingsenlim0806@gmail.com>
2021-08-14 12:33:05 +0800

    Added skeleton for users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,10 @@
 const User = require("../models/User");
 const router = require("express").Router();
 
+// add subscriptions for a user
 
+
+
+// remove subscriptions for a user
 
 module.exports = router;
